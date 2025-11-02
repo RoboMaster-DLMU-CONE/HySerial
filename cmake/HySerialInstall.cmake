3db63f72848ab11ab767f599ffdb59def4de1dde
WeaveFeather <moonbite233@gmail.com>
2025-11-02 16:00:03 +0800

    add cmake install

---
+++ cmake/HySerialInstall.cmake
@@ -0,0 +1,33 @@
+include(GNUInstallDirs)
+include(CMakePackageConfigHelpers)
+
+install(TARGETS HySerial
+        EXPORT HySerialTargets
+        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+)
+install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/include/
+        DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+)
+install(EXPORT HySerialTargets
+        FILE HySerialTargets.cmake
+        NAMESPACE HySerial::
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}
+)
+write_basic_package_version_file(
+        "${CMAKE_CURRENT_BINARY_DIR}/HySerialConfigVersion.cmake"
+        VERSION ${PROJECT_VERSION}
+        COMPATIBILITY AnyNewerVersion
+)
+configure_package_config_file(
+        "${CMAKE_CURRENT_SOURCE_DIR}/cmake/HySerialConfig.cmake.in"
+        "${CMAKE_CURRENT_BINARY_DIR}/HySerialConfig.cmake"
+        INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}
+)
+install(
+        FILES
+        "${CMAKE_CURRENT_BINARY_DIR}/HySerialConfig.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/HySerialConfigVersion.cmake"
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}
+)
