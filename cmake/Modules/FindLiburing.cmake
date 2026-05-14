5a3f7dd0b8a968120bbcfc7053b7065b55caca99
WindWeaver <moonbite233@gmail.com>
2026-05-14 13:17:44 +0800

    use native way to find liburing

--- cmake/Modules/FindLiburing.cmake
+++ cmake/Modules/FindLiburing.cmake
@@ -1,1 +1,16 @@
 find_library(LIBURING_LIBRARY NAMES uring liburing REQUIRED)
+find_path(LIBURING_INCLUDE_DIR liburing.h)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Liburing
+  REQUIRED_VARS LIBURING_LIBRARY LIBURING_INCLUDE_DIR
+)
+
+if(Liburing_FOUND AND NOT TARGET Liburing::Liburing)
+  add_library(Liburing::Liburing UNKNOWN IMPORTED)
+  set_target_properties(Liburing::Liburing PROPERTIES
+    IMPORTED_LOCATION "${LIBURING_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${LIBURING_INCLUDE_DIR}"
+  )
+endif()
+mark_as_advanced(LIBURING_LIBRARY LIBURING_INCLUDE_DIR)
