fd60f21ab54c20b2da423da72100c5e1452b2d17
WindWeaver <moonbite233@gmail.com>
2026-05-28 12:52:18 +0800

    spinlock available on arm machine

--- include/HySerial/Util/SpinLock.hpp
+++ include/HySerial/Util/SpinLock.hpp
@@ -2,38 +2,32 @@
 #define HYSERIAL_SPINLOCK_HPP
 #include <atomic>
 
-class SpinLock
-{
+class SpinLock {
 public:
-    void lock() noexcept
-    {
-        for (;;)
-        {
-            if (!lock_.exchange(true, std::memory_order_acquire))
-            {
-                return;
-            }
-            while (lock_.load(std::memory_order_relaxed))
-            {
-                __builtin_ia32_pause();
-                // Currently don't support Arm machine. Need someone help.
-                // std::this_thread::yield(); performance is bad.
-            }
-        }
+  void lock() noexcept {
+    for (;;) {
+      if (!lock_.exchange(true, std::memory_order_acquire)) {
+        return;
+      }
+      while (lock_.load(std::memory_order_relaxed)) {
+#if defined(__x86_64__) || defined(__i386__)
+        __builtin_ia32_pause();
+#elif defined(__aarch64__) || defined(__arm__)
+        __asm__ __volatile__("yield" ::: "memory");
+#endif
+      }
     }
+  }
 
-    bool try_lock() noexcept
-    {
-        return !lock_.load(std::memory_order_relaxed) && !lock_.exchange(true, std::memory_order_acquire);
-    }
+  bool try_lock() noexcept {
+    return !lock_.load(std::memory_order_relaxed) &&
+           !lock_.exchange(true, std::memory_order_acquire);
+  }
 
-    void unlock() noexcept
-    {
-        lock_.store(false, std::memory_order_release);
-    }
+  void unlock() noexcept { lock_.store(false, std::memory_order_release); }
 
 private:
-    std::atomic<bool> lock_{false};
+  std::atomic<bool> lock_{false};
 };
 
-#endif //HYSERIAL_SPINLOCK_HPP
+#endif // HYSERIAL_SPINLOCK_HPP
